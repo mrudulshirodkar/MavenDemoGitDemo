f90d368bf218844310ac5950d81533533b36f371
MrudulS <mrudulshirodkar@gmail.com>
2022-08-10 12:46:50 +0530

    second commit

--- MavenDemoGitDemo/src/main/java/com/lti/demos/TestCalc.java
+++ MavenDemoGitDemo/src/main/java/com/lti/demos/TestCalc.java
@@ -5,6 +5,8 @@ public class TestCalc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Hello");
+		System.out.println("line added");
 //		
 	}
 
